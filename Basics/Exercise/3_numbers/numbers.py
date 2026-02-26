ae6c939462ec94c3a8e3703d729fbe3f01df0b27
Kaveen <kaveensathurusinghe@gmail.com>
2026-02-26 23:05:58 +0530

    exe 3 done

---
+++ Basics/Exercise/3_numbers/numbers.py
@@ -0,0 +1,15 @@
+lenght = 92
+width = 48.8
+area = lenght * width
+print(round(area,2))
+
+packets = 9
+cost = 1.49
+cash = 20
+change = cash - (packets * cost)
+print(round(change,2), " dollars")
+
+total = 5.5 ** 2
+cost = total * 500
+print (round(cost, 3), " dollars")
+
