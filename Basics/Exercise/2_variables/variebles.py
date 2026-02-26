4bfce7fb24fd0f9654e1d4cd916d879a5f35f34d
Kaveen <kaveensathurusinghe@gmail.com>
2026-02-26 15:51:08 +0530

    exe 2 done

---
+++ Basics/Exercise/2_variables/variebles.py
@@ -0,0 +1,8 @@
+birth_year = 2002
+current_year = 2026
+age = current_year - birth_year
+print(age)
+
+first_name = "Kaveen"
+last_name = "Sathurusinghe"
+print("My name is "+ first_name + " "+last_name)
